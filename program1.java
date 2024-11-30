a668943d5ea943dd900ddc6f52a87744398992f6
Bharath chengappa <devops@gmail.com>
2024-11-30 08:58:31 +0530

    program1.java file added

---
+++ program1.java
@@ -0,0 +1,13 @@
+class program1
+{
+    static void test()
+    {
+        System.out.println("executing test()");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("program starts");
+        test();
+    }
+
+}
