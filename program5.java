9c977499a1d4552636e6cf11a4063810c36ebf6c
Bharath chengappa <devops@gmail.com>
2024-11-30 08:59:12 +0530

    all files added

---
+++ program5.java
@@ -0,0 +1,7 @@
+public class program5 {
+    static void test (int arg){
+        
+    }
+}
+
+    
---
+++ program6.java
@@ -0,0 +1,18 @@
+public class program6 
+{
+    static void push(char c)
+    {
+        System.out.println("my value : "+c);
+
+    }
+        static void play(double d)
+        {
+            System.out.println("value :+" d);
+        }
+        public static void main(String[] args) {
+            System.out.println("program starts");
+            play(3.45);
+            push ('d')
+            System.out.println("program ends");
+        }
+}
